cb6393a8119fa5f6f8effea4b0f14f278f9653ae
Sinchana S <sinchanasjain11@gmail.com>
2026-02-25 10:54:24 +0530

    Add files via upload

---
+++ ATM.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.20;
+contract simpleATM{
+    uint public balance;
+    constructor(uint initialBalance){
+        balance=initialBalance;
+    }
+    function withdraw(uint amount)external {
+        _verifyBalance(amount);
+        _updateBalance(amount);
+    }
+    function _verifyBalance(uint amount)internal view {
+        require(balance>=amount,"insufficient balance");
+    }
+    function _updateBalance(uint amount)internal {
+        balance-=amount;
+    }
+}
